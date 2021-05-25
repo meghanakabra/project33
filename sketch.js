e943cd5b35052b17bb9d1422c26cee3d65913c6a
meghanakabra <76811118+meghanakabra@users.noreply.github.com>
2021-05-25 16:51:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var snow = [];
+
+var engine,world;
+var girl,girlImg,object1,object2,object1Img,object2Img;
+function preload(){
+  backgroundImg=loadImage("snow1.jpg");
+  girlImg=loadImage("girl.png");
+  object1Img=loadImage("extra1.png");
+  object2Img=loadImage("extra2.png")
+}
+function setup() {
+  engine = Engine.create();
+  world = engine.world;
+  createCanvas(800,400);
+  girl=createSprite(400, 290, 50, 50);
+  girl.addImage(girlImg);
+  girl.scale=0.5;
+  object1=createSprite(600, 290, 50, 50);
+  object1.addImage(object1Img);
+  object1.scale=0.5;
+  object2=createSprite(200, 290, 50, 50);
+  object2.addImage(object2Img);
+  object2.scale=0.5;
+}
+
+
+function draw() {
+  background(backgroundImg);  
+  Engine.update(engine);
+  if(frameCount%20===0){
+    snow.push(new Snow(random(200,400), random(50,100),100,100));
+  }
+  for (var j = 0; j < snow.length; j++) {
+   console.log("snow display")
+    snow[j].display();
+  }
+  drawSprites();
+}
+function keyPressed(){
+  if(keyCode===32){
+    girl.y=girl.y-15;
+  }
+}
